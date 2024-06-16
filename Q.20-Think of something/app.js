4bda5f281e33d2010bd94728b456f388d4de623d
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-16 23:12:49 +0500

    Add files via upload

---
+++ Q.20-Think of something/app.js
@@ -0,0 +1,7 @@
+// Q.20 Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var languages = ["urdu", "english", "arabic", "french", "hindi"];
+console.log("list of languages: ");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var top_1 = languages_1[_i];
+    console.log(top_1);
+}
