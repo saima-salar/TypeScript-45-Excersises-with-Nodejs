61e4184cd24481829bc66071ab1cc82e30612c43
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-16 22:47:51 +0500

    Add files via upload

---
+++ Q.7-Number Eight/app.js
@@ -0,0 +1,17 @@
+// Q.7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// addition
+var a = 5;
+var b = 3;
+console.log(a + b);
+// subtraction
+var c = 11;
+var d = 3;
+console.log(c - d);
+// multiplication
+var e = 2;
+var f = 4;
+console.log(e * f);
+// division
+var g = 16;
+var h = 2;
+console.log(g / h);
