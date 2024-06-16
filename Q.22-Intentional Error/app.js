939d35e9ca0edca1a2e4c21c58d43abe5421c43d
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-16 23:16:24 +0500

    Add files via upload

---
+++ Q.22-Intentional Error/app.js
@@ -0,0 +1,6 @@
+//  Q.22 Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+// let array: (string | number)[] = [`ali`, `ahmed`, 1, 3, 5];
+// console.log(array[5]);
+// Make sure you correct the error before closing the program.
+var array = ["ali", "ahmed", 1, 3, 5, "asma"];
+console.log(array[5]);
