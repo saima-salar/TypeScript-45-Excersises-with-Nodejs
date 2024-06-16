3318e1da6cf0baff92a2baee4d4be9e49ed7af96
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-17 00:08:04 +0500

    Add files via upload

---
+++ Q.38-Cities/app.js
@@ -0,0 +1,8 @@
+// Q.38 Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+function describe_city(nameOfcity, country) {
+    if (country === void 0) { country = "Pakistan"; }
+    console.log("".concat(nameOfcity, " is in ").concat(country));
+}
+describe_city("Karachi");
+describe_city("Lahore");
+describe_city("Dubai", "UAE");
