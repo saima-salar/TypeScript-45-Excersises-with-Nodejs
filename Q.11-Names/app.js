72bf274761e49afc509fc9135f13f74d5442a44b
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-16 22:56:04 +0500

    Add files via upload

---
+++ Q.11-Names/app.js
@@ -0,0 +1,5 @@
+// Q.11 Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var members = ["umar", "rayyan", "ali", "yahya", "abeera"];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
