b17ad2ccfcd8889cd7d00659e1d2078dd414a1bf
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-16 23:36:51 +0500

    Add files via upload

---
+++ Q.24-More Conditional Tests/app.ts
@@ -0,0 +1,47 @@
+// Q.24 More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+
+// • Tests for equality and inequality with strings
+
+console.log("Equality test: ", ("Muhammad Umar" as string) === "Muhammad Umar");
+console.log("Inequality test: ", ("Muhammad Umar" as string) === "Hammad Ali");
+
+// // • Tests using the lower case function
+
+console.log(
+  "lower case test: ",
+  "Muhammad Umar".toLocaleLowerCase() === "muhammad umar"
+);
+console.log(
+  "lower case test: ",
+  "Muhammad Umar".toLocaleLowerCase() === "Muhammad Umar"
+);
+
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+console.log("Equality test: ", (1000 as number) === 1000);
+console.log("Equality test: ", (1000 as number) === 100);
+console.log("Equality test: ", (1000 as number) !== 100);
+console.log("Equality test: ", (1000 as number) !== 1000);
+console.log("Greater than test: ", 1000 > 100);
+console.log("Greater than test: ", 1000 > 10000);
+console.log("Less than test: ", 10 < 100);
+console.log("Less than test: ", 10 < 1);
+console.log("Greater than or equal to test: ", 10 >= 10);
+console.log("Greater than or equal to test: ", 10 >= 100);
+console.log("Less than or equal to test: ", 5 <= 10);
+console.log("Less than or equal to test: ", 5 <= 1);
+
+// • Tests using "and" and "or" operators
+
+console.log("and operator test", true && true);
+console.log("or operator test", true || false);
+
+// • Test whether an item is in a array
+
+const car = [1, 2, 3];
+console.log("test number", car.includes(2));
+console.log("test number", car.includes(7));
+
+// • Test whether an item is not in a array
+const car1 = [1, 2, 3];
+console.log("test number", !car1.includes(7));
+console.log("test number", !car1.includes(3));
