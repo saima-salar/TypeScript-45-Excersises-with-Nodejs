5b199d57a0732827ba7bcc4bb8d668086d3380b7
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-16 22:42:08 +0500

    Add files via upload

---
+++ Q.4-Famous Quote/app.js
@@ -0,0 +1,5 @@
+// Q.4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+var author = "Nelson Mandela";
+var qoute = "\u201CDo not judge me by my successes,judge me by how many times I fell down and got back up again.\u201D";
+console.log("".concat(author, " once said,  ").concat(qoute));
---
+++ Q.4-Famous Quote/app.ts
@@ -0,0 +1,9 @@
+// Q.4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let author: string = "Nelson Mandela";
+
+let qoute: string = `“Do not judge me by my successes,judge me by how many times I fell down and got back up again.”`;
+
+console.log(`${author} once said,  ${qoute}`);
