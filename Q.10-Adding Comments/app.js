2e1856f2b3add3213c42a2cec1c57b922b5623aa
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-16 22:53:22 +0500

    Add files via upload

---
+++ Q.10-Adding Comments/app.js
@@ -0,0 +1,6 @@
+// Q.10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// My name is Saima Salar
+// Dated: 27-05-2024
+//this program will run simple code just like Hello world
+var message = "Hello World simple Pragramme";
+console.log(message);
---
+++ Q.10-Adding Comments/app1.ts
@@ -0,0 +1,9 @@
+// Q.10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// My name is Saima Salar
+// Dated: 27-05-2024
+//this program will run simple  multiplication
+
+let num1: number = 6;
+let num2: number = 9;
+console.log(num1 * num2);
